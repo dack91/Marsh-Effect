5c8621f2c6bc355b2685fd5d37a042c9e76b1afe
Delainey Ackerman <dack91@gmail.com>
2016-11-26 04:17:53 +0000

    Add modular pieces and batch fbx export script

---
+++ scripts/fbxExport.py
@@ -0,0 +1,35 @@
+import maya.cmds as cmds
+
+kitList = cmds.ls('ship_*')
+transformList = cmds.listRelatives(kitList, parent=True, fullPath=True)
+
+exportDir = "/Users/delainey/Documents/School/Goldsmiths/Intro to Modeling and Animation/MarshEffect/modelExports/"
+
+#iterate through all transform nodes
+for part in transformList:
+    cmds.select(part)
+
+    #save original position
+    x = cmds.getAttr(str(part) + ".translateX")
+    y = cmds.getAttr(str(part) + ".translateY")
+    z = cmds.getAttr(str(part) + ".translateZ")
+    
+    #move to origin
+    cmds.setAttr(str(part) + ".translateX", 0)
+    cmds.setAttr(str(part) + ".translateY", 0)
+    cmds.setAttr(str(part) + ".translateZ", 0)
+         
+    #freeze transforms
+    cmds.makeIdentity(apply=True, t=1, r=1, s=1, n=0, pn=1)
+    
+    #delete history
+    cmds.DeleteHistory()
+    
+    #export fbx
+    exportFileName = str(exportDir) + str(part[1:])
+    cmds.file(exportFileName, force=True, options='v=0', type="FBX export", pr=True, es=True)
+    
+    #reset to previous position
+    cmds.setAttr(str(part) + ".translateX", x)
+    cmds.setAttr(str(part) + ".translateY", y)
+    cmds.setAttr(str(part) + ".translateZ", z)
